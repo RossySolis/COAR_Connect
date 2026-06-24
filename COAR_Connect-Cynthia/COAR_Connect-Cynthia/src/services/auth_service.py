562b3b4774f02b40c19acc4e1b880107081445f1
75684350 <75684350@continental.edu.pe>
2026-06-24 01:38:12 -0500

    Delete COAR_Connect-Cynthia/COAR_Connect-Cynthia directory
    hubo errores en la creación del backend y no se pudo conectar a la base datos

--- COAR_Connect-Cynthia/COAR_Connect-Cynthia/src/services/auth_service.py
+++
@@ -1,9 +0,0 @@
-from src.repository.auth_repository import AuthRepository
-from src.schema.auth import Login
-
-class AuthService:
-    def __init__(self):
-        self.authRepository = AuthRepository()
-
-    async def login(self, login: Login):
-        return await self.authRepository.login(login)
